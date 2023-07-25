702685d5346250924ba4e300513c177b564d0c3a
umbertofaenza <umberto.faenza@gmail.com>
2023-07-25 17:31:20 +0200

    feat_js: add for cycle to generate list

--- JSnack2/js/main.js
+++ JSnack2/js/main.js
@@ -1,7 +1,13 @@
 const firstNames = ["John", "Paul", "George", "Ringo"];
 const lastNames = ["Lennon", "McCartney", "Harrison", "Starr"];
 
-const randomFirstName = Math.floor(Math.random() * firstNames.length);
-const randomLastName = Math.floor(Math.random() * lastNames.length);
+const unordereList = document.getElementById("invited-list");
 
-const unorderedList = document.getElementById("invited-list");
+for (let i = 0; i < 10; i++) {
+  const randomFirstName = Math.floor(Math.random() * firstNames.length);
+  const randomLastName = Math.floor(Math.random() * lastNames.length);
+
+  unordereList.innerHTML += `
+<li>${firstNames[randomFirstName]} ${lastNames[randomLastName]}</li>
+`;
+}
