178f3e5545a263789e9e4c56eaf9b07a2ae9e9ba
umbertofaenza <umberto.faenza@gmail.com>
2023-07-25 18:08:37 +0200

    feat_js: add for cycle and print total

--- JSnack3/js/main.js
+++ JSnack3/js/main.js
@@ -1,3 +1,12 @@
 const intNumbers = [5, 10, 15, 20, 25];
 let evenIndexNumber;
 let totalNumber = 0;
+
+for (let i = 0; i < intNumbers.length; i++) {
+  if (i % 2 == 0) {
+    evenIndexNumber = intNumbers[i];
+    totalNumber += evenIndexNumber;
+  }
+}
+
+console.log(totalNumber);
