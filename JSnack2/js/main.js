c391658892908a88368f892949d433816c8a908d
umbertofaenza <umberto.faenza@gmail.com>
2023-07-25 16:56:45 +0200

    feat_js: initialize two const with random numbers

--- JSnack2/js/main.js
+++ JSnack2/js/main.js
@@ -1,2 +1,5 @@
 const firstNames = ["John", "Paul", "George", "Ringo"];
 const lastNames = ["Lennon", "McCartney", "Harrison", "Starr"];
+
+const randomFirstName = Math.floor(Math.random() * firstNames.length);
+const randomLastName = Math.floor(Math.random() * lastNames.length);
